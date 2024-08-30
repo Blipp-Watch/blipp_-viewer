ec96dccd6b1254e591dadedb27ebc23e590af485
bl4ckh401 <pavkiptoo@gmail.com>
2024-08-30 14:42:14 +0300

    add tonconnect manifest

--- src/app/Frens/page.tsx
+++ src/app/Frens/page.tsx
@@ -1,12 +1,25 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Referral from "../components/referralSystem";
 
 export default function Frens() {
     const [initData, setInitData] = useState("");
     const [userId, setUserId] = useState("");
     const [startParam, setStartParam] = useState("");
+
+    useEffect(() => {
+        const initWebApp = async () => {
+            if(typeof window !== "undefined") {
+                const WebApp = (await import("@twa-dev/sdk")).default;
+                WebApp.ready();
+                setInitData(WebApp.initData);
+                setUserId(WebApp.initDataUnsafe.user?.id.toString() || "");
+                setStartParam(WebApp.initDataUnsafe.start_param || "");
+            }
+        }
+        initWebApp();
+    }, []);
 
     return(
         <>
